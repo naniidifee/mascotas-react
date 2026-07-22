5c56d7a09a5f189c43544418e55cf50b40355e90
Marjorie Gomez <marjoriedavid2006@gmail.com>
2026-07-21 23:52:38 -0400

    primer commit del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom'
+
+function App() {
+  return (
+    <h1>Hello word</h1>
+  );
+}
+
+export default App
