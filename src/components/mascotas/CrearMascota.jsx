0e70052cc6fa6e2bd977f1ea1e9ff3152ae56721
Alisonn26 <alison.limachi@inacapmail.cl>
2026-07-24 23:35:55 -0400

    Creacion del componente para registrar mascota

---
+++ src/components/mascotas/CrearMascota.jsx
@@ -0,0 +1,10 @@
+function CrearMascota() {
+    return (
+        <div>
+            <h2>Registrar nueva mascota</h2>
+            <p>Formulario de registro proximamente...</p>
+        </div>
+    );
+}
+
+export default CrearMascota;
